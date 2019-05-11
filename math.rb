aa8dee5c09ad77531bbddbb9d03c26084df0dfcb
Jeremy Rotter <jeremy.rotter@gmail.com>
2019-05-11 08:05:51 -0400

    Prime Factorization

--- math.rb
+++ math.rb
@@ -49,28 +49,32 @@ end
 #
 def prime_factors(n)
 
-  # For all possible prime factors, see if they divide n
-  prime_factors = []
-  prime_factors << 2 if n > 1
-  i = 3
-  while i <= n do
-    is_prime = true
-    primes.each do |p|
-      if (i % p == 0)
-        is_prime = false
-        break
-      end
+  sqrt = n**(0.5)
+  all_primes = primes(sqrt)
+  factors = []
+  all_primes.each do |p|
+    power = 1
+    while n % (p**power) == 0
+      factors << p
+      power += 1
     end
-    if is_prime
-      power = 1
-      while i**power % i == 0
-        prime_factors << i
-      end
-    end
-    i += 2
   end
-  puts "PRIME FACTORS == #{prime_factors.inspect}"
-  prime_factors
+  puts "FACTORS == #{factors}"
+  factors
 end
 
 
+def combination(n,r)
+  numerator = 1
+  for i in (r+1)..n do
+    numerator = (numerator * i)
+  end
+  denominator = 1
+  for i in 1..(n-r) do
+    denominator = (denominator * i)
+  end
+  #puts "numerator == #{numerator}, denominator == #{denominator}"
+  #puts numerator / denominator
+  #puts denominator
+  (numerator / denominator)
+end
